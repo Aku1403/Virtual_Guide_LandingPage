e2eeacb18d9bda9b8d18c285a413fd33f7d88a16
Aku1403 <135806429+Aku1403@users.noreply.github.com>
2025-03-25 05:47:34 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,7 @@
+document.querySelector(".hire-btn").addEventListener("click", function() {
+    alert("Contact us for personalized travel experiences in India!");
+});
+
+document.querySelector("#exit").addEventListener("click", function() {
+    alert("Thank you for visiting! Safe travels.");
+});
